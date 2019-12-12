d317ea46cf10478534ed9ffdf3523a6e76d547e2
Vishal Patel <vishal@defsysgroup.com>
2019-12-12 15:51:14 +0530

    Add project files.

---
+++ VishalPatel/Default.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VishalPatel
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string toemail = "vishal@vishalpatel.in";
+            string toname = "Vishal Patel";
+            string fromemail = txtemail.Text.Trim();
+            string fromname = txtname.Text.Trim();
+            string body = txtbody.Text.Trim();
+            string sub = "Email from your website visit - " + txtsubject.Text.Trim();
+            var result = SendMail(fromemail, fromname, toemail, toname, body, sub, string.Empty, string.Empty, false);
+            if (result)
+            {
+                form1.Visible = false;
+                pMsg.Visible = true;
+            }
+            else
+            {
+                form1.Visible = true;
+                pMsg.Visible = false;
+            }
+        }
+
+        public bool SendMail(string FromEmail, string FromName, string ToEmails, string toname, string MailBody, string Subject, string CcEmails, string BccEmails, bool IsBodyHtml)
+        {
+            try
+            {
+                System.Net.Mail.MailMessage objMail = new System.Net.Mail.MailMessage();
+
+                //Mail From Field
+                if (!String.IsNullOrEmpty(FromName))
+                {
+                    objMail.From = new MailAddress(FromEmail, FromName);
+                    objMail.Sender = new MailAddress(FromEmail, FromName);
+                }
+                else
+                {
+                    objMail.From = new MailAddress(FromEmail);
+                    objMail.Sender = new MailAddress(FromEmail);
+                }
+
+                //Mail To Field
+                if (!String.IsNullOrEmpty(ToEmails))
+                {
+                    objMail.To.Add(ToEmails);
+                }
+                //Mail Cc Field
+                if (!String.IsNullOrEmpty(CcEmails))
+                {
+                    objMail.CC.Add(CcEmails);
+                }
+
+                //Mail Bcc Field
+                if (!String.IsNullOrEmpty(BccEmails))
+                {
+                    objMail.Bcc.Add(BccEmails);
+                }
+
+                //Mail Subject Field
+                objMail.Subject = Subject;
+
+                //Mail Body Field
+                objMail.Body = MailBody;
+                objMail.IsBodyHtml = IsBodyHtml;
+
+                try
+                {
+                    SmtpClient smtp1 = new SmtpClient();
+                    smtp1.Send(objMail);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ VishalPatel/WebForm1.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="VishalPatel.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:Button ID="btnSubmit" runat="server" Text="Send Message"  CssClass="btn btn-primary py-3 px-5" OnClick="" />
+        </div>
+    </form>
+</body>
+</html>
